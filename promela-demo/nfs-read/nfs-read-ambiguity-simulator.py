1511f0beb1e351a8bb26e90ca8697f05716cdbf3
Haolin Yu <haolin.yu@outlook.com>
2020-06-29 06:57:27 -0400

    add a simulator written by python

---
+++ promela-demo/nfs-read/nfs-read-ambiguity-simulator.py
@@ -0,0 +1,18 @@
+import random as rd
+
+def getRandom():
+    return rd.randrange(10000), rd.randrange(10000), rd.randrange(10000)
+
+if __name__ == "__main__":
+    for i in range(20):
+        if_count = 0
+        offset, count, file_len = getRandom()
+        if(offset >= file_len):
+            if_count += 1
+        if(offset + count < file_len):
+            if_count += 1
+        if(offset + count >= file_len):
+            if_count += 1
+        if(if_count != 1):
+            print("Ambiguity found with offset", offset, "count", count, "file_len", file_len)
+            break
