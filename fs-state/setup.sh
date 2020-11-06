adf2cd9b80e250d62540b81a7913978f259f87f5
Wei Su <suwei@fsl.cs.sunysb.edu>
2020-11-06 20:41:24 +0000

    Fix setup script

--- fs-state/setup.sh
+++ fs-state/setup.sh
@@ -182,14 +182,15 @@ runcmd losetup -D
 n_fs=${#FSLIST[@]};
 for i in $(seq 0 $(($n_fs-1))); do
 
-    # Mount first
+    # Run individual file system setup scripts defined above
+    fs=${FSLIST[$i]};
+    DEVICE=${DEVLIST[$i]};
+
+    # Unmount first
     if [ "$(mount | grep /mnt/test-$fs)" ]; then
         runcmd umount -f /mnt/test-$fs;
     fi
 
-    # Run individual file system setup scripts defined above
-    fs=${FSLIST[$i]};
-    DEVICE=${DEVLIST[$i]};
     setup_$fs $DEVICE;
 
     if [ -d /mnt/test-$fs ]; then
