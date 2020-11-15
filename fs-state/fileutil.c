842d48c1a63bdbbe0ca514cd892d866def724acf
Wei Su <suwei@cs.stonybrook.edu>
2020-11-14 19:38:37 -0500

    Add fsck

--- fs-state/fileutil.c
+++ fs-state/fileutil.c
@@ -223,6 +223,55 @@ void closeall()
     _n_files = 0;
 }
 
+static char *receive_output(FILE *cmdfp, size_t *length)
+{
+    const size_t block = 4096;
+    char *buffer = malloc(block);
+    size_t readsz = 0, bufsz = block;
+    assert(buffer);
+    while (!feof(cmdfp)) {
+        if (readsz + block > bufsz) {
+            char *newbuf = realloc(buffer, bufsz + block);
+            if (!newbuf) {
+                fprintf(stderr, "%s encountes out-of-memory issue, command "
+                        "output is truncated at %zu bytes.\n", __func__,
+                        readsz);
+                break;
+            }
+            bufsz += block;
+            buffer = newbuf;
+        }
+        char *ptr = buffer + readsz;
+        readsz += fread(ptr, 1, block, cmdfp);
+    }
+    *length = readsz;
+    return buffer;
+}
+
+bool do_fsck()
+{
+    char cmdbuf[ARG_MAX];
+    bool isgood = true;
+    for (int i = 0; i < N_FS; ++i) {
+        snprintf(cmdbuf, ARG_MAX, "fsck -N -t %s %s 2>&1", fslist[i],
+                 basepaths[i]);
+        FILE *cmdfp = popen(cmdbuf, "r");
+        size_t outlen = 0;
+        char *output = receive_output(cmdfp, &outlen);
+        int ret = pclose(cmdfp);
+        if (ret != 0) {
+            fprintf(stderr, "fsck %s failed and returned %d, %s may have been "
+                    "corrupted.\n", basepaths[i], ret, fslist[i]);
+            fprintf(stderr, "Here's the output: \n");
+            fwrite(output, 1, outlen, stderr);
+            fprintf(stderr, "\n");
+            isgood = false;
+        }
+        free(output);
+    }
+    return isgood;
+}
+
 void mountall()
 {
     bool inited = false;
@@ -266,6 +315,7 @@ void unmount_all()
 {
     bool has_failure = false;
     record_fs_stat();
+    assert(do_fsck());
     for (int i = 0; i < N_FS; ++i) {
         int ret = umount2(basepaths[i], 0);
         if (ret != 0) {
