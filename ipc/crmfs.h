2f0d9edf97e646b32ad1d047b69b6a6a070c0bbd
Wei Su <suwei@cs.stonybrook.edu>
2020-12-29 07:40:51 +0000

    A working checkpoint/restore memory fs (CRMFS)

--- ipc/crmfs.h
+++ ipc/crmfs.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <errno.h>
 #include <assert.h>
 #include <stdbool.h>
@@ -10,8 +11,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/limits.h>
+#include <sys/types.h>
 
-#include "vector.h"
+// #include "vector.h"
 
 #if !defined(FUSE_USE_VERSION) || FUSE_USE_VERSION < 30
 #define FUSE_USE_VERSION 30
@@ -47,7 +49,7 @@ struct crmfs_dirtable {
 
 #define CRM_BLOCK_SZ          512
 #define CRM_FILE_EXIST        1
-#define CRM_FILE_ATTR(filep)  (filep)->entry_param.attr
+#define CRM_FILE_ATTR(filep, key)  (filep)->entry_param.attr.st_##key
 #define CRM_FILE_MODE(filep)  (filep)->entry_param.attr.st_mode
 #define CRM_FILE_SIZE(filep)  (filep)->entry_param.attr.st_size
 #define CRM_DEFAULT_ICAP      10
@@ -57,5 +59,57 @@ struct crmfs_state {
   size_t nfiles;
   struct crmfs_file *files;
 };
+
+static inline void *ERR_PTR(long error)
+{
+  return (void *) error;
+}
+
+static inline long PTR_ERR(void *ptr)
+{
+  return (long) ptr;
+}
+
+#ifndef MAX_ERRNO
+#define MAX_ERRNO   4095
+#endif
+static inline bool IS_ERR(void *ptr)
+{
+  return (unsigned long) ptr >= (unsigned long) -MAX_ERRNO;
+}
+
+static inline size_t round_up(size_t n, size_t unit)
+{
+  return ((n + unit - 1) / unit) * unit;
+}
+
+static inline size_t round_down(size_t n, size_t unit)
+{
+  return round_up(n, unit) - unit;
+}
+
+static inline size_t nblocks(size_t size)
+{
+  if (size == 0) {
+    return 0;
+  } else {
+    return round_up(size, CRM_BLOCK_SZ) / CRM_BLOCK_SZ;
+  }
+}
+
+static inline void _crmfs_debug(const char *fmt, ...)
+{
+#ifdef DEBUG
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+#endif
+}
+
+#define debug(msg, ...) \
+  _crmfs_debug("%s: " msg, __func__, ##__VA_ARGS__)
+
+#define enter() _crmfs_debug("%s\n", __func__);
 
 #endif // _CRMFS_H_
