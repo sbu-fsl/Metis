efb4b3ac141011b3c74a85ecd5ccaf4244c775b4
Tejeshwar Reddy <tejeshwarreddy1996@gmail.com>
2021-11-02 12:40:43 -0400

    NFS CR: Fix nfs_cr.sh script
    - making it executable.
    - update criu command

--- fs-state/nfs_cr.sh
+++ fs-state/nfs_cr.sh
@@ -3,12 +3,12 @@
 checkpoint() {
     # get the list of open nodes on which nfs-ganesha server is running.
     local PORT=`lsof +E -aUc ganesha | grep STREAM | awk '{ print $8 }' | head -n 2 | tail -n 1`
-    mkdir -p $1
-    criu dump --tree `pgrep ganesha.nfsd` --shell-job --images-dir $1 --external unix[$PORT] --ext-unix-sk --leave-running --track-mem
+    mkdir -p nfs/$1
+    criu dump --tree `pgrep ganesha.nfsd` --shell-job --images-dir nfs/$1 --external unix[$PORT] --ext-unix-sk --leave-running --track-mem
 }
 
 restore() {
-    criu restore --shell-job --images-dir $1 --ext-unix-sk &
+    criu restore --shell-job --images-dir nfs/$1 --ext-unix-sk &
 }
 
 if [ $1 == "c" ]; then
