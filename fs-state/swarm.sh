b3604a4a1edd3b6270880e736f783e8b68651fba
Pei Liu <peiliu.cs@gmail.com>
2022-02-21 23:20:17 -0500

    remote swarm setup

--- fs-state/swarm.sh
+++ fs-state/swarm.sh
@@ -3,6 +3,7 @@
 WD=$(pwd)
 verbose=0
 num_pan=4
+remote=$(grep -Po '\t.*:\d+( |\t)' swarm.lib | awk '{print $2}' | awk -F ':' '{print $1}')
 
 runcmd() {
 	if [ "$verbose" != "0" ]; then
@@ -16,10 +17,19 @@ runcmd() {
 		exit $ret;
 	fi
 }
+
 # use for loop to run a command 4 times with different number in the command
 for i in {1..4}; do
 	runcmd make install ARGS=$i;
 done
+
+runcmd echo "copying dependencies to $remote..."
+
+scp libsmcfs1.a "$remote":libsmcfs1.a
+scp libsmcfs2.a "$remote":libsmcfs2.a
+scp libsmcfs3.a "$remote":libsmcfs3.a
+scp libsmcfs4.a "$remote":libsmcfs4.a
+
 
 runcmd swarm swarm.lib -f demo.pml
 runcmd ./demo.pml.swarm
