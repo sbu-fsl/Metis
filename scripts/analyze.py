1839e3e2ef8a8f7c5d02645df576fb1341d65f82
Wei Su <suwei@cs.stonybrook.edu>
2022-03-28 22:07:26 -0400

    Upload scripts to extract abs-states and analyze overlaps

---
+++ scripts/analyze.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import sys
+
+absfs = {
+    'pan1': [],
+    'pan2': [],
+    'pan3': [],
+    'pan4': []
+}
+
+
+def loadall():
+    for inst in absfs.keys():
+        fp = open('time-absfs-%s.log' % inst, 'r')
+        print('Loading time-absfs file for %s...' % inst, file=sys.stderr)
+        try:
+            for line in fp:
+                timestamp, state = line.split(',')
+                timestamp = float(timestamp)
+                absfs[inst].append((timestamp, state))
+        finally:
+            fp.close()
+        pass
+
+sets = {
+    'pan1': set(), 'pan2': set(),
+    'pan3': set(), 'pan4': set()
+}
+last_offset = {
+    'pan1': 0, 'pan2': 0, 'pan3': 0, 'pan4': 0
+}
+
+
+def analyze(end_secs):
+    res = {}
+    # load set
+    for inst in absfs.keys():
+        fp = open('time-absfs-%s.log' % inst, 'r')
+        fp.seek(last_offset[inst])
+        line = fp.readline()
+        while line:
+            ts, value = line.split(',')
+            ts = float(ts)
+            sets[inst].add(value)
+            if ts > end_secs:
+                break
+            line = fp.readline()
+        last_offset[inst] = fp.tell()
+        fp.close()
+    # Compute numbers
+    res['pan1 & pan2'] = len(sets['pan1'] & sets['pan2'])
+    res['pan1 U pan2'] = len(sets['pan1'] | sets['pan2'])
+    res['pan1 & pan3'] = len(sets['pan1'] & sets['pan3'])
+    res['pan1 U pan3'] = len(sets['pan1'] | sets['pan3'])
+    res['pan1 & pan4'] = len(sets['pan1'] & sets['pan4'])
+    res['pan1 U pan4'] = len(sets['pan1'] | sets['pan4'])
+    res['pan2 & pan3'] = len(sets['pan2'] & sets['pan3'])
+    res['pan2 U pan3'] = len(sets['pan2'] | sets['pan3'])
+    res['pan2 & pan4'] = len(sets['pan2'] & sets['pan4'])
+    res['pan2 U pan4'] = len(sets['pan2'] | sets['pan4'])
+    res['pan3 & pan4'] = len(sets['pan3'] & sets['pan4'])
+    res['pan3 U pan4'] = len(sets['pan3'] | sets['pan4'])
+    res['all_intersect'] = len(sets['pan1'] & sets['pan2'] & sets['pan3'] & sets['pan4'])
+    res['all_union'] = len(sets['pan1'] | sets['pan2'] | sets['pan3'] | sets['pan4'])
+    res['by_twomore'] = len(
+        (sets['pan1'] & sets['pan2']) |
+        (sets['pan1'] & sets['pan3']) |
+        (sets['pan1'] & sets['pan4']) |
+        (sets['pan2'] & sets['pan3']) |
+        (sets['pan2'] & sets['pan4']) |
+        (sets['pan3'] & sets['pan4'])
+    )
+    return res
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: %s <max-hours>' % sys.argv[0])
+        exit(1)
+
+    max_hours = int(sys.argv[1])
+    # load time-absfs files
+    # loadall()
+    # print header
+    print('time_secs,pan1 & pan2,pan1 U pan2,overlap_pan1_pan2,', end='')
+    print('pan1 & pan3,pan1 U pan3,overlap_pan1_pan3,', end='')
+    print('pan1 & pan4,pan1 U pan4,overlap_pan1_pan4,', end='')
+    print('pan2 & pan3,pan2 U pan3,overlap_pan2_pan3,', end='')
+    print('pan2 & pan4,pan2 U pan4,overlap_pan2_pan4,', end='')
+    print('pan3 & pan4,pan3 U pan4,overlap_pan3_pan4,', end='')
+    print('total_unique_states,partial_overlap,partial_waste_ratio,', end='')
+    print('total_overlap,total_waste_ratio')
+    # compute each hour
+    for hr in range(max_hours):
+        ts = (hr + 1) * 3600
+        res = analyze(ts)
+        # print fields
+        print('%d,%d,%d,%.6f' % (ts, res['pan1 & pan2'], res['pan1 U pan2'], res['pan1 & pan2'] / res['pan1 U pan2']), end=',')
+        print('%d,%d,%.6f' % (res['pan1 & pan3'], res['pan1 U pan3'], res['pan1 & pan3'] / res['pan1 U pan3']), end=',')
+        print('%d,%d,%.6f' % (res['pan1 & pan4'], res['pan1 U pan4'], res['pan1 & pan4'] / res['pan1 U pan4']), end=',')
+        print('%d,%d,%.6f' % (res['pan2 & pan3'], res['pan2 U pan3'], res['pan2 & pan3'] / res['pan2 U pan3']), end=',')
+        print('%d,%d,%.6f' % (res['pan2 & pan4'], res['pan2 U pan4'], res['pan2 & pan4'] / res['pan2 U pan4']), end=',')
+        print('%d,%d,%.6f' % (res['pan3 & pan4'], res['pan3 U pan4'], res['pan3 & pan4'] / res['pan3 U pan4']), end=',')
+        print('%d,%d,%.6f' % (res['all_union'], res['by_twomore'], res['by_twomore'] / res['all_union']), end=',')
+        print('%d,%.6f' % (res['all_intersect'], res['all_intersect'] / res['all_union']))
+
---
+++ scripts/extract_absfs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import re
+import subprocess as sp
+
+from sys import argv
+
+absfs_pat = re.compile(r'\[\s*(\d+\.\d+)\] absfs = \{([0-9a-z]+)\}')
+
+
+def get_filelist(pattern='.'):
+    p = sp.Popen('ls -tr %s' % pattern, shell=True, stdout=sp.PIPE)
+    ret = p.wait()
+    if ret != 0:
+        raise Exception('Listing failed, return value is %d' % ret)
+    return p.stdout.read().decode('utf-8').splitlines()
+
+
+if __name__ == '__main__':
+    # File name pattern for `ls` command
+    if len(argv) < 2:
+        print('Usage: %s <filename-pattern>' % argv[0])
+        exit(1)
+
+    fnpattern = argv[1]
+    flist = get_filelist(fnpattern)
+
+    for fpath in flist:
+        fp = open(fpath, 'r')
+        try:
+            for line in fp:
+                result = absfs_pat.match(line)
+                if result is None:
+                    continue
+                timestamp = result.group(1)
+                absfs = result.group(2)
+                print('%s,%s' % (timestamp, absfs))
+        finally:
+            fp.close()
