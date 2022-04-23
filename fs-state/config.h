b6288e0b6a55a91b2b6dda3486dd21fdb5920f7e
Yifei Liu <yifeliu@cs.stonybrook.edu>
2022-04-23 18:59:09 -0400

    Remove unnecessary legacy config header files

--- fs-state/config.h
+++ fs-state/config.h
@@ -17,27 +17,6 @@
 /* Abort the whole program when expect() fails */
 #define ABORT_ON_FAIL    1
 
-/* List of file systems: Modify this to experiment with other file systems */
-// static const char *fslist[] = {"ext4", "jffs2"};
-// static const char *fssuffix[] = {"", ""};
-// static const char *devlist[] = {"/dev/ram0", "/dev/mtdblock0"};
-// static const size_t devsize_kb[] = {256, 256};
-// #define N_FS    nelem(devsize_kb)
-// #define N_FS    2
-// char *basepaths[N_FS];
-// char *testdirs[N_FS];
-// char *testfiles[N_FS];
-
-/* Pointer to memory-mapped file system images */
-// void *fsimgs[N_FS];
-/* File descriptors of the opened f/s images */
-// int fsfds[N_FS];
-
-// absfs_state_t absfs[N_FS];
-
-// FILE *seqfp; // looks like it's not being used
-
-//int rets[N_FS], errs[N_FS];
 int i;
 
 #endif // _FSSTATE_CONFIG_H_
