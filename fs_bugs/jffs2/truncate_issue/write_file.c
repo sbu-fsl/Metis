983a1e1c161cce6cdad9d9ff8977166cb6eff3a6
Erez Zadok <ezk@cs.sunysb.edu>
2022-05-21 16:12:56 -0400

    cleanup code

--- fs_bugs/jffs2/truncate_issue/write_file.c
+++ fs_bugs/jffs2/truncate_issue/write_file.c
@@ -5,53 +5,45 @@
 #include <fcntl.h>
 #include <string.h>
 
-int write_file(const char *path, off_t offset, size_t length, int byte)
+/* write to path, at offset, for len bytes, value "byte" */
+int main(int argc, char *argv[])
 {
-    int ret = -1, err = -1;
-    int fd = open(path, O_RDWR);
-    if (fd < 0) {
-        return ret;
-    }
-    off_t res = lseek(fd, offset, SEEK_SET);
-    if (res == (off_t) -1) {
-        err = errno;
-        goto exit;
-    }
-
-    char *data = malloc(length);
-    if (data == NULL) {
-        err = errno;
-        goto exit;
-    }
-    memset(data, byte, length);
-
-    ssize_t writesz = write(fd, data, length);
-    if (writesz < 0) {
-        err = errno;
-        goto exit;
-    }
-    if (writesz < length) {
-        fprintf(stderr, "Note: less data written than expected (%ld < %zu)\n",
-                writesz, length);
-    }
-    ret = writesz;
-
-exit:
-    close(fd);
-    errno = err;
-    return ret;
-}
-
-int main(int argc, void **argv)
-{
-    char *file = argv[1];
+    char *path = argv[1];
     int offset = atoi(argv[2]);
-    int len = atoi(argv[3]);
+    ssize_t len = atoi(argv[3]);
     int byte = atoi(argv[4]);
 
-    int ret = write_file(file, offset, len, byte);
-    if (ret == -1) {
-        perror("Error while writing to file\n");
+    int fd = open(path, O_RDWR);
+    if (fd < 0) {
+      perror(path);
+      exit(1);
     }
-    return ret;
+
+    off_t res = lseek(fd, offset, SEEK_SET);
+    if (res < 0) {
+      perror("lseek");
+      exit(1);
+    }
+
+    char *data = malloc(len);
+    if (data == NULL) {
+      errno = ENOMEM;
+      perror("malloc");
+      exit(1);
+    }
+    memset(data, byte, len);
+
+    ssize_t writesz = write(fd, data, len);
+    if (writesz < 0) {
+      perror("write");
+      exit(1);
+    }
+    if (writesz < len) {
+      fprintf(stderr, "Note: less data written than expected (%ld < %ld)\n",
+	      writesz, len);
+      exit(1);
+    }
+
+    close(fd);
+    exit(0);
 }
