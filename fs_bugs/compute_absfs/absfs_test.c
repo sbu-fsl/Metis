79e1b6894a7f38dfcf8a12ed5131522d92050cd0
Yifei Liu <yifeliu@cs.stonybrook.edu>
2022-08-14 07:12:03 +0000

    Extract abstract computing function

--- fs_bugs/compute_absfs/absfs_test.c
+++ fs_bugs/compute_absfs/absfs_test.c
@@ -1,45 +1,51 @@
 #include "absfs_test.h"
 
-char *mp = NULL;
-absfs_state_t absfs;
-
-static void compute_abstract_state(const char *basepath,
-    absfs_state_t state)
-{
-    absfs_t absfs;
-
-    absfs.hash_option = absfs_hash_method;
-    init_abstract_fs(&absfs);
-    scan_abstract_fs(&absfs, basepath, false, submit_error);
-    memcpy(state, absfs.state, sizeof(absfs_state_t));
-    destroy_abstract_fs(&absfs);
-}
-
 char *get_abstract_state(const char *basepath,
-    absfs_state_t state)
+    unsigned int hash_method, char *abs_state_str)
 {
-    compute_abstract_state(basepath, state);
-    char abs_state_str[33] = {0};
+    int ret;
+    absfs_t absfs;
+    absfs.hash_option = hash_method;
+    init_abstract_fs(&absfs);
+    ret = scan_abstract_fs(&absfs, basepath, false, printf);
+
+    if (ret) {
+        printf("Error occurred when computing absfs...\n");
+        return NULL;
+    }
+
     char *strp = abs_state_str;
     for (int i = 0; i < 16; ++i) {
-        size_t res = snprintf(strp, 3, "%02x", absfs[0][i]);
+        size_t res = snprintf(strp, 3, "%02x", absfs.state[i]);
         strp += res;
     }
+    destroy_abstract_fs(&absfs);
     return abs_state_str;
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <mountpoint>\n", argv[0]);
+    char *mp = NULL;
+    // By default it is 0 for xxh128_t 
+    unsigned int hash_option = 0;
+    if (argc > 1) {
+        mp = argv[1];
+        if(argc > 2){
+            hash_option = argv[2][0] - '0';
+            if (hash_option > 3) {
+                fprintf(stderr, "Hash option not supported.\n");
+            }
+        }
+    }
+    else {
+        fprintf(stderr, "Usage: %s <mountpoint> [hash_option]\n", argv[0]);
         exit(1);
     }
-    mp = argv[1];
-    absfs = calloc(1, sizeof(absfs_state_t));
 
-    char *absfs_ret = compute_abstract_state(mp, absfs);
+    char *abs_state_str = calloc(33, sizeof(char));
+    char *absfs_ret = get_abstract_state(mp, hash_option, abs_state_str);
     printf("%s\n", absfs_ret);
+    free(abs_state_str);
 
-    free(absfs);
     return 0;
 }
--- fs_bugs/compute_absfs/absfs_test.h
+++ fs_bugs/compute_absfs/absfs_test.h
@@ -1,8 +1,10 @@
 #include "abstract_fs.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef _ABSFS_TEST_H
 #define _ABSFS_TEST_H
 
-char *get_abstract_state(const char *basepath, absfs_state_t state);
+char *get_abstract_state(const char *basepath, unsigned int hash_method, char *abs_state_str);
 
 #endif // _ABSFS_TEST_H
