5e9be471cad833c0266e9de0564b97e1d994b291
Yifei Liu <yifeliu@cs.stonybrook.edu>
2022-09-27 21:26:33 +0000

    Restore previous replayer while creating a new replayer that dumps images

--- fs-state/replay_dump.c
+++ fs-state/replay_dump.c
@@ -15,6 +15,11 @@
 #include "operations.h"
 #include "vector.h"
 
+#define DUMP_REPLAY_IMAGES 1
+
+// The seq number to dump images (dump images only if seqid greater or equal to this value)
+#define SEQID_DUMP_THRESHOLD 17126800
+
 int seq = 0;
 
 typedef struct concrete_state {
@@ -23,6 +28,102 @@ typedef struct concrete_state {
 } fs_state_t;
 
 vector_t states;
+
+/* Replayer dump device utilities */
+static size_t state_depth = 0;
+
+static int ensure_dump_dir(const char *folder)
+{
+    struct stat st;
+    int ret = stat(folder, &st);
+    /* Try creating the folder if it doesn't exist */
+    if (ret < 0 && errno == ENOENT) {
+        ret = mkdir(folder, 0755);
+        if (ret < 0) {
+            fprintf(stderr, "[ERROR]: %s: cannot create folder %s (%s)\n", __func__,
+                    folder, errnoname(errno));
+            return -errno;
+        }
+    } else if (ret < 0) {
+        fprintf(stderr, "[ERROR]: %s: failed to stat %s, error is %s\n", __func__,
+                folder, errnoname(errno));
+        return -errno;
+    } else {
+        if (!S_ISDIR(st.st_mode)) {
+            errno = ENOTDIR;
+            fprintf(stderr, "[ERROR]: %s: folder %s is not a directory.\n", __func__,
+                    folder);
+            return -ENOTDIR;
+        }
+    }
+    return 0;
+}
+
+
+static void dump_replay_mmaped(const char *folder, int i, char *fs_img)
+{
+	char outpath[PATH_MAX] = {0};
+	snprintf(outpath, PATH_MAX, "%s/%s-mmap-%d-%zu.img", folder,
+            get_fslist()[i], seq, state_depth);
+    const size_t bs = 4096;
+    int dmpfd = open(outpath, O_CREAT | O_RDWR | O_TRUNC, 0666);
+    if (dmpfd < 0) {
+        fprintf(stderr, "[ERROR]: cannot create file %s", outpath);
+        return;
+    }
+
+	int fsfd = open(get_devlist()[i], O_RDWR);
+    size_t remaining = fsize(fsfd);
+    char *ptr = fs_img;
+    while (remaining > 0) {
+        size_t writelen = (remaining >= bs) ? bs : remaining;
+        ssize_t writeres = write(dmpfd, ptr, writelen);
+        if (writeres < 0) {
+            fprintf(stderr, "[ERROR]: cannot write data to image dump %s", outpath);
+            close(dmpfd);
+            break;
+        }
+        ptr += writeres;
+        remaining -= writeres;
+    }
+    close(dmpfd);
+	close(fsfd);
+}
+
+static void dump_replay_device(const char *folder, int i)
+{
+	char *devname = get_devlist()[i];
+	char *fsname = get_fslist()[i];
+
+    char cmd[ARG_MAX] = {0};
+    snprintf(cmd, ARG_MAX, "dd if=%s of=%s/%s-dev-%d-%zu.img bs=4k status=none",
+             devname, folder, fsname, seq, state_depth);
+    int status = system(cmd);
+    if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "[ERROR]: Cannot dump %s on %s, dd exited with %d.",
+                    fsname, devname, WEXITSTATUS(status));
+        }
+    } else if (WIFSIGNALED(status)) {
+        fprintf(stderr, "[ERROR]: Cannot dump %s on %s, dd was terminated by signal "
+                "%d.", fsname, devname, WTERMSIG(status));
+    } else {
+        fprintf(stderr, "[ERROR]: Cannot dump %s on %s, dd has exit code %d.",
+                fsname, devname, status);
+    }
+}
+
+static void dump_replay_fs_images(const char *folder, char **fs_imgs)
+{
+    assert(ensure_dump_dir(folder) == 0);
+    for (int i = 0; i < get_n_fs(); ++i) {
+        /* Dump the mmap'ed object */
+        dump_replay_mmaped(folder, i, fs_imgs[i]);
+        /* Dump the device by direct copying */
+        dump_replay_device(folder, i);
+    }
+
+}
 
 void extract_fields(vector_t *fields_vec, char *line, const char *delim)
 {
@@ -171,11 +272,18 @@ void checkpoint()
 	fs_state_t state;
 	state.seqid = seq;
 	state.images = calloc(get_n_fs(), sizeof(char *));
+
+	printf("checkpoint (the state seqid = %d, state_depth = %zu)\n", seq, state_depth);
+	state_depth++;
+
 	for (int i = 0; i < get_n_fs(); ++i) {
 		do_checkpoint(get_devlist()[i], &state.images[i]);
 	}
 	vector_add(&states, &state);
-	printf("checkpoint\n");
+
+	// dump checkpoint image here
+	if (seq >= SEQID_DUMP_THRESHOLD)
+		dump_replay_fs_images("replay-before-checkpoint", state.images);
 }
 
 static void do_restore(const char *devpath, char *buffer)
@@ -189,7 +297,6 @@ static void do_restore(const char *devpath, char *buffer)
 	assert(ptr != MAP_FAILED);
 	
 	memcpy(ptr, buffer, size);
-        free(buffer);
 
 	munmap(ptr, size);
 	close(devfd);
@@ -200,18 +307,28 @@ void restore()
 	fs_state_t *state = vector_peek_top(&states, fs_state_t);
 	if (!state)
 		return;
+
+	printf("restore (to the state just before seqid = %d, state_depth = %zu)\n", state->seqid, state_depth);
+	state_depth--;
+
 	for (int i = 0; i < get_n_fs(); ++i) {
 		do_restore(get_devlist()[i], state->images[i]);
+	}
+	// dump restored image here
+	if (seq >= SEQID_DUMP_THRESHOLD)
+		dump_replay_fs_images("replay-after-restore", state->images);
+	for (int i = 0; i < get_n_fs(); ++i) {
+		if (state->images[i])
+			free(state->images[i]);
 	}
 	if (state->images)
 		free(state->images);
 	vector_pop_back(&states);
-	printf("restore (to the state just before seqid = %d)\n", state->seqid);
 }
 
 int main(int argc, char **argv)
 {
-	FILE *seqfp = fopen("sequence.log", "r");
+	FILE *seqfp = fopen("sequence-pan-20220522-182011-2443643.log", "r");
 	ssize_t len;
 	size_t linecap = 0;
 	char *linebuf = NULL;
