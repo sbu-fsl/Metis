6761f15008ed6e574240cf9adae298cc7027b03c
Yifei Liu <yifeliu@cs.stonybrook.edu>
2022-11-14 00:13:16 -0500

    Modify gcov ext4 script to make ext4 support journaling and print runtime

--- fs-state/mcfs_scripts/gcov_ext4.sh
+++ fs-state/mcfs_scripts/gcov_ext4.sh
@@ -4,7 +4,8 @@ SERVER=sgdp05
 FSCHECKER=mcfs
 
 KERNEL_EXT4_SRC="/sys/kernel/debug/gcov/mcfs/Linux_Kernel_Install/linux-6.0.6/fs/ext4"
-EXT4_SZKB=2048
+# 8192 - 8 MiB/16 MiB device for ext4 auto enables journaling
+EXT4_SZKB=8192
 CURDIR=$PWD
 
 cd ..
@@ -14,6 +15,13 @@ sudo rmmod brd
 
 modprobe brd rd_nr=1 rd_size=$EXT4_SZKB
 sudo lcov --zerocounters
+
+start=`date +%s`
 ./setup.sh -f ext4:$EXT4_SZKB
+end=`date +%s`
+runtime=$((end-start))
+
 lcov --capture --directory $KERNEL_EXT4_SRC --rc lcov_branch_coverage=1 --output-file $CURDIR/${SERVER}_${FSCHECKER}_ext4_coverage.info
 genhtml $CURDIR/${SERVER}_${FSCHECKER}_ext4_coverage.info --rc genhtml_branch_coverage=1 --output-directory $CURDIR/${SERVER}_${FSCHECKER}_cov_out
+
+echo "Total Ext4 MCFS Tests Runtime: " $runtime
