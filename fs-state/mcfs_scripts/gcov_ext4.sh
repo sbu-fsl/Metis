a3c12464e83b66d578c6d9fecd96b9444d0ed143
Yifei Liu <yifeliu@cs.stonybrook.edu>
2022-12-22 18:12:51 +0000

    Several gcov scripts for MCFS

---
+++ fs-state/mcfs_scripts/gcov_ext4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SERVER=sgdp05
+FSCHECKER=mcfs
+SUFFIX="journal60mins_VFS"
+
+KERNEL_EXT4_SRC="/sys/kernel/debug/gcov/mcfs/Linux_Kernel_Install/linux-6.0.6/fs"
+# 8192 - 8 MiB device for ext4 AUTO enables journaling (doesn't work: 4/5/6/7 MiB)
+# sudo dumpe2fs /dev/ram0 | grep has_journal: check if ext4 journaling enabled
+EXT4_SZKB=8192
+CURDIR=$PWD
+OUTPUT_INFO="$CURDIR/gcov_results/${SERVER}_${FSCHECKER}_ext4_coverage_${SUFFIX}.info"
+OUTPUT_DIR="$CURDIR/gcov_results/${SERVER}_${FSCHECKER}_cov_out_${SUFFIX}"
+
+cd ..
+sudo ./stop.sh
+sudo rmmod brd
+# sudo ./loadmods.sh
+
+modprobe brd rd_nr=1 rd_size=$EXT4_SZKB
+sudo lcov --zerocounters
+
+start=`date +%s`
+./setup.sh -f ext4:$EXT4_SZKB
+end=`date +%s`
+runtime=$((end-start))
+
+lcov --capture --directory $KERNEL_EXT4_SRC --rc lcov_branch_coverage=1 --output-file $OUTPUT_INFO
+genhtml $OUTPUT_INFO --rc genhtml_branch_coverage=1 --output-directory $OUTPUT_DIR
+
+echo "Total Ext4 MCFS Tests Runtime: " $runtime
