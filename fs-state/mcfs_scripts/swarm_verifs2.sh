4b8adf191fd873b521cb99f7bb60541ffbf3f320
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-07-21 20:22:10 -0400

    Ext4 vs VeriFS2 swarm script

--- fs-state/mcfs_scripts/swarm_verifs2.sh
+++ fs-state/mcfs_scripts/swarm_verifs2.sh
@@ -7,15 +7,24 @@
 # Make sure the swarm.lib is set up
 # Make sure the VeriFS2 is installed
 
+# For long experiment: we can do "rm -r test-ext4-i* && rm -r test-verifs2-i*" first
+
 NUMVT=6
 
-EXT4_SZKB=256
+# EXT4_SZKB=256
+
+EXT4_SZKB=512
+
 VERIFS2_SZKB=0 
 
 cd ..
 sudo ./stop.sh
 sudo rmmod brd
 
-./loadmods.sh
+# Ext4: 256 KiB (change EXT4_SZKB)
+# ./loadmods.sh
+
+# Ext4: 512 KiB (change EXT4_SZKB)
+./loadlargebrds.sh
 
 ./setup_swarm.sh -f ext4:$EXT4_SZKB:verifs2:$VERIFS2_SZKB -n $NUMVT
