d661b9958d95029769e8a850c25415fe20a2d8be
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-07-28 16:22:33 -0400

    Complete script to create symlinks

--- driver-fs-state/driver_scripts/create_driver_symlinks.sh
+++ driver-fs-state/driver_scripts/create_driver_symlinks.sh
@@ -9,11 +9,23 @@
 # Others:   whichconfig.h 
 
 # FILES REQUIRED BY THE DRIVER
-# config.h fileutil.{c,h} init_globals.{c,h} load*.sh mcfs-main.pml
-# mount.c perf.c set.{cpp,h} setup.{c,h} setup*.sh stop.sh swarm.lib*
+# config.h fileutil.c fileutil.h init_globals.c init_globals.h loadlargebrds.sh loadmods.sh mcfs-main.pml
+# mount.c parameter_util.py perf.c set.cpp set.h setup.c setup.h setup.sh setup_swarm.sh stop.sh swarm.lib swarm.lib.copy
 
 # FILES SHOULD BE UNIQUE FOR DRIVER 
-# Makefile parameters.py parameter_util.py
+# Makefile parameters.py
 
+SYMLK_FILES="config.h fileutil.c fileutil.h init_globals.c init_globals.h loadlargebrds.sh loadmods.sh mcfs-main.pml mount.c parameter_util.py perf.c set.cpp set.h setup.c setup.h setup.sh setup_swarm.sh stop.sh swarm.lib swarm.lib.copy"
 
+cd ../../fs-state
+
+FILES_ARR=( $(ls $SYMLK_FILES) )
+
+cd ../driver-fs-state
+# For each checker file, create a symbolic link in driver-fs-state/driver_scripts
+for CFILE in "${FILES_ARR[@]}"; do
+    ln -s ../fs-state/$CFILE $CFILE
+done
+
+echo "Symlinks created from checker (fs-state) to driver (driver-fs-state)"
 
