6e715971f5025d7a76e5982159de026309ed70d6
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-10-12 16:03:30 -0400

    Rename absfs csv files for easier analysis

--- scripts/multi_machines_analysis/run_multi_analysis.sh
+++ scripts/multi_machines_analysis/run_multi_analysis.sh
@@ -10,7 +10,6 @@ EACH_VT_CNT=6
 MASTER_DIR="/mcfs-swarm/nfs4mc/fs-state"
 SWARM_SCRIPT_DIR="/mcfs-swarm/nfs4mc/scripts/multi_machines_analysis"
 
-
 ################ Part 1: For each machine, go to logs directory, and decompress output-pan*.log.gz
 
 # Function to decompress .gz files that are not yet decompressed on a given node
@@ -71,7 +70,7 @@ wait
 
 echo "All abstract states extraction jobs finished."
 
-# ################ Part 3: Copy extracted abstract states from client machines to master machine
+################ Part 3: Copy extracted abstract states from client machines to master machine
 
 ## Copy time-absfs-{hostname}-VT{allVT}-pan(VTnum).csv from clients to the master
 for CLIENT in "${CLIENTS[@]}"; do
@@ -81,3 +80,30 @@ done
 wait
 
 echo "All abstract states csv copying jobs finished."
+
+################ Part 4: Rename abstract states csv files for analysis 
+
+## Correct total VT count (e.g., 18 which is 3 machines * 6 VTs per machine)
+TOTVM=$(((${#CLIENTS[@]} + 1) * EACH_VT_CNT))
+
+rename "s/VT$EACH_VT_CNT/VT$TOTVM/" time-absfs-*-VT$EACH_VT_CNT-pan*.csv
+
+for index in "${!CLIENTS[@]}"; do
+    CLIENT="${CLIENTS[$index]}"
+    for file in time-absfs-$CLIENT-VT*-pan*.csv; do
+        # Extract the pan number before .csv using awk (e.g., time-absfs-yifeilatest4-VT6-pan3.csv, num=3)
+        num=$(echo "$file" | awk -F'pan' '{print $2}' | cut -d '.' -f1)
+        # Increment the number by 6 every time
+        new_num=$((num + 6 * (index + 1)))
+        # Construct the new filename using bash's string replacement
+        new_file="${file/pan$num.csv/pan$new_num.csv}"
+        # Rename the file
+        mv "$file" "$new_file"    
+    done 
+done
+
+## Rename to master hostname
+for CLIENT in "${CLIENTS[@]}"; do
+    rename "s/$CLIENT/$MASTER/" time-absfs-$CLIENT-VT*-pan*.csv
+done 
+
