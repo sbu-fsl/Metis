621f5699dabe693e06face37f3bdf11cba417f8f
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-10-11 22:15:54 -0400

    Check if the file is already decompressed before while processing swarm logs

--- scripts/multi_machines_analysis/run_multi_analysis.sh
+++ scripts/multi_machines_analysis/run_multi_analysis.sh
@@ -7,19 +7,44 @@ CLIENTS=("yifeilatest4" "yifeilatest5")
 
 CLIENT_DIR="/mcfs-swarm"
 EACH_VT_CNT=6
+MASTER_DIR="/mcfs-swarm/nfs4mc/fs-state"
 CLIENT_SWARM_DIR="/mcfs-swarm/nfs4mc/scripts/multi_machines_analysis"
+
 
 # Part 1: For each machine, go to logs directory, and decompress output-pan*.log.gz
 
-# Function to decompress .gz files on a given node
+# Function to decompress .gz files that are not yet decompressed on a given node
 decompress_on_node() {
     local node=$1
-    ssh "$node" "find ${CLIENT_DIR} -name 'output-pan*.log.gz' -exec gunzip -k {} \;"
+    ssh $node "
+        for gzfile in \$(find $CLIENT_DIR -name 'output-pan*.log.gz'); do
+            file_without_gz=\${gzfile%.gz}
+            if [ ! -f \$file_without_gz ]; then
+                gunzip -k \$gzfile
+            else
+                echo \"\$gzfile is already decompressed\"
+            fi
+        done
+    "
 }
 
-## For master machine, decompress at local fs-state directory
-cd ../../fs-state 
-gzip -d output-pan*.log.gz &
+## For master machine, decompress at local fs-state directory (MASTER_DIR)
+
+# For each .gz file in the directory
+for gzfile in "$MASTER_DIR"/output-pan*.log.gz; do
+    # Check if the .gz file exists (this handles the case where the glob doesn't match any files)
+    if [[ -f $gzfile ]]; then
+        # Extract the filename without the .gz extension
+        decompressed_file="${gzfile%.gz}"
+        # Check if the decompressed file exists
+        if [[ ! -f "$decompressed_file" ]]; then
+            # Decompress the file without deleting the original .gz file in background
+            gunzip -k "$gzfile" &
+        else
+            echo "$gzfile is already decompressed"
+        fi
+    fi
+done    
 
 ## For client machines, decompress at remote mcfs-swarm directory
 for CLIENT in "${CLIENTS[@]}"; do
@@ -31,17 +56,18 @@ wait
 
 echo "All decompression jobs finished."
 
-# Part 2: Extract abstract states from raw output log files
+# # Part 2: Extract abstract states from raw output log files
 
-## Master machine
-cd /mcfs-swarm/nfs4mc/scripts/multi_machines_analysis
-python3 multi_extract_absfs.py $EACH_VT_CNT ../../fs-state/output-pan\*.log &
+# ## Master machine (local)
+# cd /mcfs-swarm/nfs4mc/scripts/multi_machines_analysis    
 
-## Client machines
-## For client machines, extract abstract states from logs remotely
-for CLIENT in "${CLIENTS[@]}"; do
-    ssh $CLIENT "cd $CLIENT_SWARM_DIR && python3 multi_extract_absfs.py $EACH_VT_CNT ../../fs-state/output-pan\*.log" &
-done
+# python3 multi_extract_absfs.py $EACH_VT_CNT ../../fs-state/output-pan\*.log &
 
-# Wait for all background absfs extraction jobs to finish
-wait
+# ## Client machines (remote via ssh)
+# ## For client machines, extract abstract states from logs remotely
+# for CLIENT in "${CLIENTS[@]}"; do
+#     ssh $CLIENT "cd $CLIENT_SWARM_DIR && python3 multi_extract_absfs.py $EACH_VT_CNT ../../fs-state/output-pan\*.log" &
+# done
+
+# # Wait for all background absfs extraction jobs to finish
+# wait
