28e3ac2df9df175895f8d4a515cb38ed107fd1e5
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-10-12 01:48:00 -0400

    Abstract state extraction for distributed Swarm VTs

--- scripts/multi_machines_analysis/run_multi_analysis.sh
+++ scripts/multi_machines_analysis/run_multi_analysis.sh
@@ -32,24 +32,21 @@ decompress_on_node() {
 
 # For each .gz file in the directory
 for gzfile in "$MASTER_DIR"/output-pan*.log.gz; do
-    # Check if the .gz file exists (this handles the case where the glob doesn't match any files)
-    if [[ -f $gzfile ]]; then
-        # Extract the filename without the .gz extension
-        decompressed_file="${gzfile%.gz}"
-        # Check if the decompressed file exists
-        if [[ ! -f "$decompressed_file" ]]; then
-            # Decompress the file without deleting the original .gz file in background
-            gunzip -k "$gzfile" &
-        else
-            echo "$gzfile is already decompressed"
-        fi
+    # Extract the filename without the .gz extension
+    file_without_gz="${gzfile%.gz}"
+    # Check if the decompressed file exists
+    if [[ ! -f "$file_without_gz" ]]; then
+        # Decompress the file without deleting the original .gz file in background
+        gunzip -k "$gzfile" &
+    else
+        echo "$gzfile is already decompressed"
     fi
-done    
-
-## For client machines, decompress at remote mcfs-swarm directory
-for CLIENT in "${CLIENTS[@]}"; do
-    decompress_on_node $CLIENT &
 done
+
+# ## For client machines, decompress at remote mcfs-swarm directory
+# for CLIENT in "${CLIENTS[@]}"; do
+#     decompress_on_node $CLIENT &
+# done
 
 # Wait for all background decompression jobs to finish
 wait
@@ -66,11 +63,13 @@ python3 multi_extract_absfs.py $EACH_VT_CNT ../../fs-state/output-pan\*.log &
 ## Client machines (remote via ssh)
 ## For client machines, extract abstract states from logs remotely
 for CLIENT in "${CLIENTS[@]}"; do
-    ssh $CLIENT "cd $SWARM_SCRIPT_DIR && python3 multi_extract_absfs.py $EACH_VT_CNT ../../fs-state/output-pan\*.log" &
+    ssh $CLIENT "cd $SWARM_SCRIPT_DIR && python3 multi_extract_absfs.py $EACH_VT_CNT ../../../output-pan\*.log" &
 done
 
 # Wait for all background absfs extraction jobs to finish
 wait
 
-################ Part 3: Copy extracted abstract states from client machines to master machine
+echo "All abstract states extraction jobs finished."
+
+# ################ Part 3: Copy extracted abstract states from client machines to master machine
 
