05534c12c765e9fb1a1a4512b791478f1b0920bd
Gautam Ahuja <ahuja.gautam@gmail.com>
2023-11-05 14:30:27 -0500

    Abstract state checking

--- fs_bugs/nova/inconsistent_file_size/run_driver.sh
+++ fs_bugs/nova/inconsistent_file_size/run_driver.sh
@@ -5,6 +5,9 @@ DRIVER_LOOP_MAX=100000000
 NOVA_MNT_DIR="/mnt/novatest"
 PMEM_DEVICE="/dev/pmem0"
 
+EXT4_MNT_DIR="/mnt/ext4test"
+EXT4_DEVICE="/dev/ram0"
+
 if test -n "$(mount | grep $PMEM_DEVICE)" ; then
     umount $PMEM_DEVICE || exit $?
 fi
@@ -14,4 +17,4 @@ if test -d $NOVA_MNT_DIR ; then
 fi
 mkdir -p $NOVA_MNT_DIR
 
-./driver $NOVA_MNT_DIR $PMEM_DEVICE ./cbuf-nova-state-3221-seq-9968-ckpt-0.img $DRIVER_LOOP_MAX
+./driver $NOVA_MNT_DIR $PMEM_DEVICE ./cbuf-nova-state-3221-seq-9968-ckpt-0.img $EXT4_MNT_DIR $EXT4_DEVICE ./cbuf-ext4-state-3221-seq-9968-ckpt-0.img $DRIVER_LOOP_MAX
