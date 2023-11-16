98ee93b34f86a56b6cf4aedf5e6b64b2945d5b5f
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-11-15 22:23:46 -0500

    complete ftrace workflow

--- fs_bugs/nilfs2/unmount-hanging-after-create-existing-file/reproducer_ftrace.sh
+++ fs_bugs/nilfs2/unmount-hanging-after-create-existing-file/reproducer_ftrace.sh
@@ -4,26 +4,32 @@ MNTPNT="/mnt/test-nilfs2"
 IMGFILE="./nilfs2-dev-88-unmount-hanging.img"
 FTRACE_DIR="/sys/kernel/debug/tracing"
 
-# Recompile NILFS2 kernel module
-# ./recompile_nilfs2_mod.sh
+#################### Enable ftrace tracing ####################
+# ftrace step 1: disable tracing
+sudo bash -c 'echo 0 > '$FTRACE_DIR'/tracing_on'
 
-# Set up Function Filters
-# All nilfs2 functions and down_write/up_write functions
+# ftrace step 2: set ftrace function tracer
+sudo bash -c 'echo function > '$FTRACE_DIR'/current_tracer'
+
+# ftrace step 3: set up Function Filters including all nilfs2 functions and down_write/up_write functions
 sudo bash -c 'echo nilfs_* > '$FTRACE_DIR'/set_ftrace_filter'
 sudo bash -c 'echo down_write >> '$FTRACE_DIR'/set_ftrace_filter'
 sudo bash -c 'echo up_write >> '$FTRACE_DIR'/set_ftrace_filter'
 
-# Enable function tracer
-sudo bash -c 'echo function > '$FTRACE_DIR'/current_tracer'
+# ftrace step 4: Tracing specific PID
+sudo bash -c 'echo $$ > '$FTRACE_DIR'/set_ftrace_pid'
 
-# Tracing specific PID
-sudo bash -c 'echo $$ > set_ftrace_pid'
-
-# Targetted tracing (trace events related to a specific command)
+# ftrace step 5: Enable Tracing and Targetted tracing (trace events related to a specific command)
 sudo bash -c 'echo 1 > '$FTRACE_DIR'/tracing_on'
 
-# Clear the trace
-sudo sh -c 'echo 0 > '$FTRACE_DIR'/trace'
+# ftrace step (optional): Clear the trace
+# sudo sh -c 'echo 0 > '$FTRACE_DIR'/trace'
+
+# ftrace step 6: Collect Trace Data for operations
+#################### Pre-hanging steps ####################
+
+# Recompile NILFS2 kernel module
+# ./recompile_nilfs2_mod.sh
 
 # Mount an existing image to create a NILFS2 filesystem
 ./mount_nilfs2_img.sh $MNTPNT $IMGFILE
@@ -33,14 +39,18 @@ cd $MNTPNT/d-00
 touch f-02
 cd -
 
-echo "Start Unmounting..."
-##### Enable ftrace tracing
+echo "Start ftrace and problematic Unmounting..."
 
 # Unmounting to reproduce hanging
 umount $MNTPNT
 
-# Disable ftrace tracing
+# ftrace step 7: Disable Tracing After Collection
 sudo bash -c 'echo 0 > '$FTRACE_DIR'/tracing_on'
 sudo bash -c 'echo nop > '$FTRACE_DIR'/current_tracer'
+
+# ftrace step 8: Clear Settings
+
+sudo bash -c 'echo > '$FTRACE_DIR'/set_ftrace_filter'
+sudo bash -c 'echo > '$FTRACE_DIR'/set_ftrace_pid'
 
 echo "Reproducer finished (not supposed to see this message due to hanging)."
