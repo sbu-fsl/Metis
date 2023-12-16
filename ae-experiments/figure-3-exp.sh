b59a8e6686b9a3cd3124105ac29ffdebc9e7a1dd
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-12-16 00:14:48 -0500

    fig3 complete

--- ae-experiments/figure-3-exp.sh
+++ ae-experiments/figure-3-exp.sh
@@ -2,6 +2,7 @@
 
 ########### Figure 3: Input coverage for open flags (40 mins) ###########
 IOCOV_MCFS_DIR="../../IOCov/MCFS"
+TERMTIME="40m"
 
 cd ../driver-fs-state
 
@@ -14,7 +15,7 @@ cd driver_scripts
 ./only_one_ext4.sh & 
 
 # Wait for 40 minutes
-sleep 1m
+sleep $TERMTIME
 
 # Check if the script is still running and kill it if necessary
 echo "[Metis-Uniform open flags]: Time limit reached. Terminating the Metis."
@@ -32,12 +33,69 @@ cd $IOCOV_MCFS_DIR
 
 python3 parser-mcfs-log-input.py `ls sequence-pan-*.log`
 
+rm sequence-pan-*.log
+
+cd -
 ########### Metis-RSD, current in driver_scripts
 # pwd: driver_scripts
 
+make clean
 
+make MY_OPEN_FLAG_PATTERN=4
 
+cd driver_scripts
+./only_one_ext4.sh & 
 
+# Wait for 40 minutes
+sleep $TERMTIME
+
+# Check if the script is still running and kill it if necessary
+echo "[Metis-RSD open flags]: Time limit reached. Terminating the Metis."
+
+# Back to driver_scripts folder
+cd ..
+
+./stop.sh
+
+# pwd: driver_scripts
+
+cp sequence-pan-*.log $IOCOV_MCFS_DIR
+
+cd $IOCOV_MCFS_DIR
+
+python3 parser-mcfs-log-input.py `ls sequence-pan-*.log`
+
+rm sequence-pan-*.log
+
+cd -
 ########### Metis-IRSD
 
+make clean
 
+make MY_OPEN_FLAG_PATTERN=5
+
+cd driver_scripts
+./only_one_ext4.sh & 
+
+# Wait for 40 minutes
+sleep $TERMTIME
+
+# Check if the script is still running and kill it if necessary
+echo "[Metis-IRSD open flags]: Time limit reached. Terminating the Metis."
+
+# Back to driver_scripts folder
+cd ..
+
+./stop.sh
+
+# pwd: driver_scripts
+
+cp sequence-pan-*.log $IOCOV_MCFS_DIR
+
+cd $IOCOV_MCFS_DIR
+
+python3 parser-mcfs-log-input.py `ls sequence-pan-*.log`
+
+rm sequence-pan-*.log
+
+cd -
