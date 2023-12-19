6d34aa1ee3d41b9893cee2f1749dc324241dcf96
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-12-19 00:08:35 -0500

    Support Metis-XD and IXD scripts

--- ae-experiments/figure-4-exp.sh
+++ ae-experiments/figure-4-exp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CURRENT_DIR=$(pwd)
+TERMTIME="40m"
 
+CURRENT_DIR=$(pwd)
+IOCOV_MCFS_DIR="../../IOCov/MCFS"
 
 ########### Figure 4: Input coverage for write sizes (40 mins) ###########
-IOCOV_MCFS_DIR="../../IOCov/MCFS"
-TERMTIME="1m"
 
 cd ../driver-fs-state
 
@@ -63,7 +63,7 @@ for file in mcfs*_input_coords.pkl; do
 done
 
 # Plot the figure
-python3 plot-mcfs-input-write-sizes.py $CURRENT_PATTERN-$TERMTIME $INPUT_COORDS_FILE
+python3 plot-mcfs-input-write-sizes-40mins.py $CURRENT_PATTERN-$TERMTIME $INPUT_COORDS_FILE
 
 echo "Completed plotting for $CURRENT_PATTERN-$TERMTIME, check ./IOCov/MCFS folder"
 
@@ -74,6 +74,8 @@ cd $CURRENT_DIR
 ########### Metis-XD, current in driver_scripts
 # pwd: driver_scripts
 
+CURRENT_PATTERN="write-size-Metis-XD"
+./cleanup-iocov.sh
 make clean
 
 make MY_WRITE_SIZE_PATTERN=1
@@ -85,8 +87,8 @@ cd driver_scripts
 sleep $TERMTIME
 
 # Check if the script is still running and kill it if necessary
-echo "[Metis-RSD open flags]: Time limit reached. Terminating the Metis."
-
+echo "[$CURRENT_PATTERN]: Time limit reached. Terminating the Metis."
+# kill $SCRIPT_PID
 # Back to driver_scripts folder
 cd ..
 
@@ -102,9 +104,42 @@ python3 parser-mcfs-log-input.py `ls sequence-pan-*.log`
 
 rm sequence-pan-*.log
 
-cd -
-########### Metis-IXD
+PLOTNAME=""
+for file in input-cov-mcfs-*.pkl; do
+    PLOTNAME=$(echo "$file" | cut -d '-' -f 3- | sed 's/\..*$//')
+    cp "$file" "../src/unfilter-$file"
+    mv "$file" "../src/"
+done
 
+cd ../src/
+
+python3 iocov-main.py $PLOTNAME --no-parse --plot -i
+
+mv mcfs*_input_coords.pkl ../MCFS/
+
+# back to ../../IOCov/MCFS folder
+cd -
+
+INPUT_COORDS_FILE=""
+
+for file in mcfs*_input_coords.pkl; do
+    INPUT_COORDS_FILE=$file
+done
+
+# Plot the figure
+python3 plot-mcfs-input-write-sizes-40mins.py $CURRENT_PATTERN-$TERMTIME $INPUT_COORDS_FILE
+
+echo "Completed plotting for $CURRENT_PATTERN-$TERMTIME, check ./IOCov/MCFS folder"
+
+cp *.pdf $CURRENT_DIR
+
+cd $CURRENT_DIR
+
+########### Metis-IXD, current in driver_scripts
+# pwd: driver_scripts
+
+CURRENT_PATTERN="write-size-Metis-IXD"
+./cleanup-iocov.sh
 make clean
 
 make MY_WRITE_SIZE_PATTERN=2
@@ -116,8 +151,8 @@ cd driver_scripts
 sleep $TERMTIME
 
 # Check if the script is still running and kill it if necessary
-echo "[Metis-IRSD open flags]: Time limit reached. Terminating the Metis."
-
+echo "[$CURRENT_PATTERN]: Time limit reached. Terminating the Metis."
+# kill $SCRIPT_PID
 # Back to driver_scripts folder
 cd ..
 
@@ -133,6 +168,36 @@ python3 parser-mcfs-log-input.py `ls sequence-pan-*.log`
 
 rm sequence-pan-*.log
 
+PLOTNAME=""
+for file in input-cov-mcfs-*.pkl; do
+    PLOTNAME=$(echo "$file" | cut -d '-' -f 3- | sed 's/\..*$//')
+    cp "$file" "../src/unfilter-$file"
+    mv "$file" "../src/"
+done
+
+cd ../src/
+
+python3 iocov-main.py $PLOTNAME --no-parse --plot -i
+
+mv mcfs*_input_coords.pkl ../MCFS/
+
+# back to ../../IOCov/MCFS folder
 cd -
 
+INPUT_COORDS_FILE=""
+
+for file in mcfs*_input_coords.pkl; do
+    INPUT_COORDS_FILE=$file
+done
+
+# Plot the figure
+python3 plot-mcfs-input-write-sizes-40mins.py $CURRENT_PATTERN-$TERMTIME $INPUT_COORDS_FILE
+
+echo "Completed plotting for $CURRENT_PATTERN-$TERMTIME, check ./IOCov/MCFS folder"
+
+cp *.pdf $CURRENT_DIR
+
+cd $CURRENT_DIR
+
+###########
 echo "Figure 4: Input coverage for write sizes (40 mins) done."
