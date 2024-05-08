5298d3daff3ddea8d750674b0f02d51bac5d5134
Gautam Ahuja <ahuja.gautam@gmail.com>
2024-05-08 01:28:57 -0400

    Script to mount_unmount nfs in a loop

--- fs_bugs/nfs/mount_unmount/mount_driver.sh
+++ fs_bugs/nfs/mount_unmount/mount_driver.sh
@@ -3,8 +3,7 @@
 SERVER_MNT_DIR="/mnt/server";
 LOCAL_MNT_DIR="/mnt/local";
 
-EXT4_RAMDISK="/dev/sdb1";
-
+EXT4_RAMDISK="/dev/ram1";
 
 setup() {
     if test -n "$(mount | grep $SERVER_MNT_DIR)" ; then
@@ -29,43 +28,48 @@ setup() {
     chmod 755 $SERVER_MNT_DIR
     chmod 755 $LOCAL_MNT_DIR
 
-    systemctl restart nfs-kernel-server
+    if systemctl is-active --quiet nfs-kernel-server; then
+    echo "NFS kernel server is already running."
+    else
+    echo "Starting NFS kernel server..."
+    systemctl start nfs-kernel-server
+    sleep 20
+    echo "NFS kernel server started."
+    fi
 
     MKFS_FLAGS="-F -v -E lazy_itable_init=0,lazy_journal_init=0"
     mkfs.ext4 ${MKFS_FLAGS} $EXT4_RAMDISK || exit $?
 }
 
-
 # Run driver
 setup
 
-#systemctl start nfs-kernel-server
-mount -t ext4 $EXT4_RAMDISK /mnt/server
+loop_max=10
 
-#exportfs -o sync,no_root_squash,rw,insecure,fsid=0 localhost:/
-exportfs -o rw,sync,no_root_squash localhost:/mnt/server
-mount -t nfs -o rw,sync,vers=3,proto=tcp localhost:/mnt/server /mnt/local
-#mount -t nfs4 -o rw,sync localhost:/mnt/server /mnt/local
-df -H
+for ((i=1; i<=$loop_max; i++)); do
+    echo "----- Loop id: $i ----- "
+        
+    mount -t ext4 $EXT4_RAMDISK $SERVER_MNT_DIR
 
-date > /mnt/local/date.txt
-ls -l /mnt/local
-sleep 5
+    exportfs -o rw,sync,no_root_squash localhost:$SERVER_MNT_DIR
 
-umount /mnt/local
-exportfs -v -f -u localhost:/mnt/server
-# echo /mnt/server > /proc/fs/nfsd/unlock_filesystem
-#systemctl stop nfs-kernel-server
-lsof /mnt/server
-lsof $EXT4_RAMDISK
-fuser -vm /mnt/server
-#mount
+    mount -t nfs -o rw,nolock,vers=3,proto=tcp localhost:$SERVER_MNT_DIR $LOCAL_MNT_DIR
 
-for i in `seq 1 60`; do
-    if umount /mnt/server ; then break; fi
-    echo SLEEP 5s, try again...
-    #fuser -vm /mnt/server
-    sleep 5
+    date > $LOCAL_MNT_DIR/date.txt
+
+    umount $LOCAL_MNT_DIR
+    exportfs -u localhost:$SERVER_MNT_DIR
+
+    while true; do
+    umount $SERVER_MNT_DIR
+
+    if [ $? -eq 0 ]; then
+        echo "Unmount succeeded"
+        break
+    else
+        echo "Unmount failed, sleeping for 5 seconds"
+        sleep 5
+    fi
+    done
+
 done
-fuser -vm /mnt/server
-df -H
