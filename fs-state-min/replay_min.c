1e073b09fcfb434a48d4f84a4e5ca5aaab575107
Divyaank Tiwari <dtiwari@cs.stonybrook.edu>
2024-05-21 21:29:46 -0400

    Moved setup_min.c functions in replay._min.c

--- fs-state-min/replay_min.c
+++ fs-state-min/replay_min.c
@@ -19,6 +19,7 @@
 #include <assert.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <ftw.h>
 
 #include "vector.h"
@@ -256,6 +257,192 @@ void unmount_all(bool strict)
         exit(1);
 }
 
+
+static void execute_cmd(const char *cmd)
+{
+    int retval = system(cmd);
+    int status, signal = 0;
+    if ((status = WEXITSTATUS(retval)) != 0) {
+        fprintf(stderr, "Command `%s` failed with %d.\n", cmd, status);
+    }
+    if (WIFSIGNALED(retval)) {
+        signal = WTERMSIG(retval);
+        fprintf(stderr, "Command `%s` terminated with signal %d.\n", cmd,
+                signal);
+    }
+    if (status || signal) {
+        exit(1);
+    }
+}
+
+int execute_cmd_status(const char *cmd)
+{
+    int retval = system(cmd);
+    int status = WEXITSTATUS(retval);
+    return status;
+}
+
+static int check_device(const char *devname, const size_t exp_size_kb)
+{
+    int fd = open(devname, O_RDONLY);
+    struct stat devinfo;
+    if (fd < 0) {
+        fprintf(stderr, "Cannot open %s (err=%s, %d)\n",
+                devname, strerror(errno), errno);
+        return -errno;
+    }
+    int retval = fstat(fd, &devinfo);
+    if (retval < 0) {
+        fprintf(stderr, "Cannot stat %s (err=%s, %d)\n",
+                devname, strerror(errno), errno);
+        close(fd);
+        return -errno;
+    }
+    if (!S_ISBLK(devinfo.st_mode)) {
+        fprintf(stderr, "%s is not a block device.\n", devname);
+        close(fd);
+        return -ENOTBLK;
+    }
+    size_t devsize = fsize(fd);
+    if (devsize < exp_size_kb * 1024) {
+        fprintf(stderr, "%s is smaller than expected (expected %zu KB, "
+                "got %zu).\n", devname, exp_size_kb, devsize / 1024);
+        close(fd);
+        return -ENOSPC;
+    }
+    close(fd);
+    return 0; 
+}
+
+static void populate_mountpoints()
+{
+    char check_mount_cmdbuf[PATH_MAX];
+    char unmount_cmdbuf[PATH_MAX];
+    char check_mp_exist_cmdbuf[PATH_MAX];
+    char rm_mp_cmdbuf[PATH_MAX];
+    char mk_mp_cmdbuf[PATH_MAX];
+    for (int i = 0; i < get_n_fs(); ++i) {
+        snprintf(check_mount_cmdbuf, PATH_MAX, "mount | grep %s", get_basepaths()[i]);    
+        /* If the mountpoint has fs mounted, then unmount it */
+        if (execute_cmd_status(check_mount_cmdbuf) == 0) {
+            snprintf(unmount_cmdbuf, PATH_MAX, "umount -f %s", get_basepaths()[i]);
+            execute_cmd(unmount_cmdbuf);
+        }
+        /* 
+         * Caveat: if we use file/dir pools and test in-memory file systems
+         * like VeriFS, we should not remove the mount point here because
+         * we need to pre-create files/dirs in the pool. Removing mountpoints
+         * simply erase the precreated files/dirs.
+         *
+         * Also, we cannot mount VeriFS and other in-memory file systems on
+         * a non-empty mount point.
+         * 
+         * The correct way would be removing and recreating mount point of 
+         * VeriFS in the setup shell scripts before running pan.
+         */
+
+        snprintf(mk_mp_cmdbuf, PATH_MAX, "mkdir -p %s", get_basepaths()[i]);
+        execute_cmd(mk_mp_cmdbuf);
+    }
+}
+
+static int setup_jfs(const char *devname, const size_t size_kb)
+{
+    int ret;
+    char cmdbuf[PATH_MAX];
+    // Expected >= 16 MiB
+    ret = check_device(devname, 16 * 1024);
+    if (ret != 0)
+    {
+        fprintf(stderr, "Cannot %s because %s is bad or not ready.\n",
+                __FUNCTION__, devname);
+        return ret;
+    }
+    // fill the device with zeros
+    snprintf(cmdbuf, PATH_MAX,
+             "dd if=/dev/zero of=%s bs=1k count=%zu",
+             devname, size_kb);
+    execute_cmd(cmdbuf);
+    // format the device with the specified file system
+    snprintf(cmdbuf, PATH_MAX, "mkfs.jfs -f %s", devname);
+    execute_cmd(cmdbuf);
+
+    return 0;
+}
+
+void setup_filesystems()
+{
+    int ret;
+    populate_mountpoints();
+
+    for (int i = 0; i < get_n_fs(); ++i) {
+        if (strcmp(get_fslist()[i], "jfs") == 0) {
+            ret = setup_jfs(get_devlist()[i], get_devsize_kb()[i]);
+        }
+    }
+    
+    if (ret != 0)
+    {
+        fprintf(stderr, "Cannot setup JFS file system (ret = %d)\n", ret);
+        exit(1);
+    }
+}
+
+int mkdir_p(const char *path, mode_t dir_mode, mode_t file_mode)
+{
+    const size_t len = strlen(path);
+    char _path[PATH_MAX];
+    char *p; 
+
+    errno = 0;
+
+    /* Copy string so its mutable */
+    if (len > sizeof(_path)-1) {
+        errno = ENAMETOOLONG;
+        return -1; 
+    }   
+    strcpy(_path, path);
+
+    bool next_f = false;
+    bool next_d = false;
+    /* Iterate the string */
+    for (p = _path + 1; *p; p++) {
+        if (*p == '/') {
+            /* Temporarily truncate */
+            *p = '\0';
+            if (mkdir(_path, dir_mode) != 0) {
+                if (errno != EEXIST) {
+                    return -1; 
+                }
+            }
+            
+            *p = '/';
+
+            if (*(p + 1) == 'f')
+                next_f = true;
+            else if (*(p + 1) == 'd')
+                next_d = true;
+        }
+    }
+    if (next_f) {
+        int fd = creat(_path, file_mode);
+        if (fd >= 0) {
+            close(fd);
+        }
+        else if (errno != EEXIST) {
+            return -1;
+        }
+    }
+    if (next_d) {
+        if (mkdir(_path, dir_mode) != 0) {
+            if (errno != EEXIST) {
+                return -1; 
+            }
+        }
+    }
+
+    return 0;
+}
 /* 
  * NOTE: NEED TO RECOMPILE REPLAYER "make replayer" every time we run it.
  *
