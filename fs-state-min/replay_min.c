1a5daa15e7a4497344a36b95eef15ff361b9cfa4
Divyaank Tiwari <dtiwari@cs.stonybrook.edu>
2024-05-21 21:22:23 -0400

    Integrated mount functions in replay

--- fs-state-min/replay_min.c
+++ fs-state-min/replay_min.c
@@ -8,12 +8,9 @@
  * You can redistribute it and/or modify it under the terms of the Apache
  * License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
  */
-
-// #include "replayutil_min.h"
-#include <string.h>
-// #include "replayutil_min.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <stdbool.h>
 #include <errno.h>
@@ -22,35 +19,15 @@
 #include <assert.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
-#define __USE_XOPEN_EXTENDED 1
 #include <ftw.h>
 
 #include "vector.h"
-
 #include "fileutil_min.h" // includes "abstract_fs.h"
 #include "operations.h"
 
-#define ABSFS_STR_LEN 33
-
-// typedef struct concrete_state {
-// 	int seqid;
-// 	char **images;
-// } fs_state_t;
-
-// void extract_fields(vector_t *fields_vec, char *line, const char *delim);
-// void destroy_fields(vector_t *fields_vec);
-// int do_create_file(vector_t *argvec);
-// int do_write_file(vector_t *argvec, int seq);
-// int do_unlink(vector_t *argvec);
-// int do_mkdir(vector_t *argvec);
-// int do_rmdir(vector_t *argvec);
-// void populate_replay_basepaths();
-// char *get_replayed_absfs(const char *basepath, unsigned int hash_method, char *abs_state_str);
-// void execute_cmd(const char *cmd);
-
 int pre = 0;
 int seq = 0;
+
 void extract_fields(vector_t *fields_vec, char *line, const char *delim)
 {
 	vector_init(fields_vec, char *);
@@ -181,6 +158,102 @@ char *get_replayed_absfs(const char *basepath,
     }
     destroy_abstract_fs(&absfs);
     return abs_state_str;
+}
+
+void mountall()
+{
+    int failpos, err;
+    for (int i = 0; i < get_n_fs(); ++i) {
+        int ret = -1;
+        ret = mount(get_devlist()[i], get_basepaths()[i], get_fslist()[i], MS_NOATIME, "");     
+        if (ret != 0) {
+            failpos = i;
+            err = errno;
+            goto err;
+        }
+    }
+    return;
+err:
+    /* undo mounts */
+    for (int i = 0; i < failpos; ++i) {
+        umount2(get_basepaths()[i], MNT_FORCE);
+    }
+    fprintf(stderr, "Could not mount file system %s in %s at %s (%s)\n",
+            get_fslist()[failpos], get_devlist()[failpos], get_basepaths()[failpos],
+            strerror(err));
+    exit(1);
+}
+
+static void save_lsof()
+{
+    int ret;
+    static int report_count = 0;
+    char progname[NAME_MAX] = {0};
+    char logname[NAME_MAX] = {0};
+    char cmd[PATH_MAX] = {0};
+
+    get_progname(progname);
+    add_ts_to_logname(logname, NAME_MAX, "lsof", progname, "");
+    ret = snprintf(cmd, PATH_MAX, "lsof > %s-%d.txt", logname, report_count++);
+    assert(ret >= 0);
+    ret = system(cmd);
+}
+
+void unmount_all(bool strict)
+{
+    bool has_failure = false;
+    int ret;
+#ifndef NO_FS_STAT
+    record_fs_stat();
+#endif
+    for (int i = 0; i < get_n_fs(); ++i) {
+        // Change retry limit from 20 to 19 to avoid excessive delay
+        int retry_limit = 19;
+        int num_retries = 0;
+        /* We have to unfreeze the frozen file system before unmounting it.
+         * Otherwise the system will hang! */
+        /*
+        if (fs_frozen[i]) {
+            fsthaw(get_fslist()[i], get_devlist()[i], get_basepaths()[i]);
+        }
+        */
+    
+        while (retry_limit > 0) {
+            ret = umount2(get_basepaths()[i], 0);
+            if (ret == 0) {
+                break; // Success, exit the retry loop
+            }        
+
+            /* If unmounting failed due to device being busy, again up to
+            * retry_limit times with 100 * 2^n ms (n = num_retries) */
+            if (errno == EBUSY) {
+                // 100 * (1 <<  0) = 100ms
+                // 100 * (1 << 18) = 100 * 262144 = 26.2144s
+                useconds_t waitms = 100 * (1 << num_retries); // Exponential backoff starting at 100ms
+                fprintf(stderr, "File system %s mounted on %s is busy. Retry %d times,"
+                        "unmounting after %dms.\n", get_fslist()[i], get_basepaths()[i], num_retries + 1,
+                        waitms);
+                usleep(1000 * waitms);
+                num_retries++;
+                retry_limit--;
+                save_lsof();
+            } 
+            else {
+                // Handle non-EBUSY errors immediately without retrying
+                fprintf(stderr, "Could not unmount file system %s at %s (%s)\n",
+                        get_fslist()[i], get_basepaths()[i], strerror(errno));
+                has_failure = true;
+            }
+        }
+        
+        if (retry_limit == 0) {
+            fprintf(stderr, "Failed to unmount file system %s at %s after retries.\n",
+                    get_fslist()[i], get_basepaths()[i]);
+            has_failure = true;
+        }
+    }
+    if (has_failure && strict)
+        exit(1);
 }
 
 /* 
