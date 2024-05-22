85f21a8697321ad40e1dd3ac990d68d05aa5c1f3
Divyaank Tiwari <dtiwari@cs.stonybrook.edu>
2024-05-21 20:25:45 -0400

    Unified replayutil.c and replay.c

--- fs-state-min/replay_min.c
+++ fs-state-min/replay_min.c
@@ -9,10 +9,179 @@
  * License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
  */
 
-#include "replayutil_min.h"
+// #include "replayutil_min.h"
+#include <string.h>
+// #include "replayutil_min.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define __USE_XOPEN_EXTENDED 1
+#include <ftw.h>
+
+#include "vector.h"
+
+#include "fileutil_min.h" // includes "abstract_fs.h"
+#include "operations.h"
+
+#define ABSFS_STR_LEN 33
+
+// typedef struct concrete_state {
+// 	int seqid;
+// 	char **images;
+// } fs_state_t;
+
+// void extract_fields(vector_t *fields_vec, char *line, const char *delim);
+// void destroy_fields(vector_t *fields_vec);
+// int do_create_file(vector_t *argvec);
+// int do_write_file(vector_t *argvec, int seq);
+// int do_unlink(vector_t *argvec);
+// int do_mkdir(vector_t *argvec);
+// int do_rmdir(vector_t *argvec);
+// void populate_replay_basepaths();
+// char *get_replayed_absfs(const char *basepath, unsigned int hash_method, char *abs_state_str);
+// void execute_cmd(const char *cmd);
 
 int pre = 0;
 int seq = 0;
+void extract_fields(vector_t *fields_vec, char *line, const char *delim)
+{
+	vector_init(fields_vec, char *);
+	char *field = strtok(line, delim);
+	while (field) {
+		size_t flen = strlen(field);
+		char *field_copy = malloc(flen + 1);
+		assert(field_copy);
+		field_copy[flen] = '\0';
+		strncpy(field_copy, field, flen);
+		vector_add(fields_vec, &field_copy);
+		field = strtok(NULL, ", ");
+	}
+}
+
+void destroy_fields(vector_t *fields_vec)
+{
+	char **field;
+	vector_iter(fields_vec, char *, field) {
+		free(*field);
+	}
+	vector_destroy(fields_vec);
+}
+
+int do_create_file(vector_t *argvec)
+{
+	char *filepath = *vector_get(argvec, char *, 1);
+	char *flagstr = *vector_get(argvec, char *, 2);
+	char *modestr = *vector_get(argvec, char *, 3);
+	char *endptr;
+	int flags = (int)strtol(flagstr, &endptr, 8);
+	int mode = (int)strtol(modestr, &endptr, 8);
+	int res = create_file(filepath, flags, mode);
+	printf("create_file(%s, 0%o, 0%o) -> ret=%d, errno=%s\n",
+	       filepath, flags, mode, res, strerror(errno));
+	return res;
+}
+
+int do_write_file(vector_t *argvec, int seq)
+{
+	char *filepath = *vector_get(argvec, char *, 1);
+	char *flagstr = *vector_get(argvec, char *, 2);
+	char *offset_str = *vector_get(argvec, char *, 4);
+	char *len_str = *vector_get(argvec, char *, 5);
+	char *endp;
+	int flags = (int)strtol(flagstr, &endp, 8);
+	off_t offset = strtol(offset_str, &endp, 10);
+	size_t writelen = strtoul(len_str, &endp, 10);
+	assert(offset != LONG_MAX);
+	assert(writelen != ULONG_MAX);
+	
+	char *buffer = malloc(writelen);
+	assert(buffer != NULL);
+	/* This is to make sure data written to all file systems in the same
+	 * group of operations is the same */
+	int integer_to_write = seq / get_n_fs();
+	generate_data(buffer, writelen, offset, BYTE_REPEAT, integer_to_write);
+	int ret = write_file(filepath, flags, buffer, offset, writelen);
+	int err = errno;
+	printf("write_file(%s, %o, %ld, %lu) -> ret=%d, errno=%s\n",
+	       filepath, flags, offset, writelen, ret, strerror(err));
+	free(buffer);
+	return ret;
+}
+
+int do_unlink(vector_t *argvec)
+{
+	char *path = *vector_get(argvec, char *, 1);
+	int ret = unlink(path);
+	int err = errno;
+	printf("unlink(%s) -> ret=%d, errno=%s\n",
+	       path, ret, strerror(err));
+	return ret;
+}
+
+int do_mkdir(vector_t *argvec)
+{
+	char *path = *vector_get(argvec, char *, 1);
+	char *modestr = *vector_get(argvec, char *, 2);
+	char *endp;
+	int mode = (int)strtol(modestr, &endp, 8);
+	int ret = mkdir(path, mode);
+	int err = errno;
+	printf("mkdir(%s, 0%o) -> ret=%d, errno=%s\n",
+	       path, mode, ret, strerror(err));
+	return ret;
+}
+
+int do_rmdir(vector_t *argvec)
+{
+	char *path = *vector_get(argvec, char *, 1);
+	int ret = rmdir(path);
+	int err = errno;
+	printf("rmdir(%s) -> ret=%d, errno=%s\n",
+	       path, ret, strerror(err));
+	return ret;
+}
+
+void populate_replay_basepaths()
+{
+	for (int i = 0; i < get_n_fs(); ++i) {
+		size_t len = snprintf(NULL, 0, "/mnt/test-%s%s", 
+								get_fslist()[i], get_fssuffix()[i]);
+		get_basepaths()[i] = calloc(1, len + 1);
+		snprintf(get_basepaths()[i], len + 1, "/mnt/test-%s%s", 
+								get_fslist()[i], get_fssuffix()[i]);
+	}
+}
+
+char *get_replayed_absfs(const char *basepath,
+    unsigned int hash_method, char *abs_state_str)
+{
+    int ret;
+    absfs_t absfs;
+    absfs.hash_option = hash_method;
+    init_abstract_fs(&absfs);
+    ret = scan_abstract_fs(&absfs, basepath, false, printf);
+
+    if (ret) {
+        printf("Error occurred when computing absfs...\n");
+        return NULL;
+    }
+
+    char *strp = abs_state_str;
+    for (int i = 0; i < 16; ++i) {
+        size_t res = snprintf(strp, 3, "%02x", absfs.state[i]);
+        strp += res;
+    }
+    destroy_abstract_fs(&absfs);
+    return abs_state_str;
+}
 
 /* 
  * NOTE: NEED TO RECOMPILE REPLAYER "make replayer" every time we run it.
