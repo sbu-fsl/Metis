dffeebf985701a6ada8fddd6b3c43ec62e8e8234
Divyaank Tiwari <divyaankt21@gmail.com>
2024-05-26 18:44:52 -0400

    Add Copyright notice

--- fs-state/loop_replay.sh
+++ fs-state/loop_replay.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#
+# Copyright (c) 2020-2024 Yifei Liu
+# Copyright (c) 2020-2024 Divyaank Tiwari
+# Copyright (c) 2020-2024 Erez Zadok
+# Copyright (c) 2020-2024 Stony Brook University
+# Copyright (c) 2020-2024 The Research Foundation of SUNY
+#
+# You can redistribute it and/or modify it under the terms of the Apache License, 
+# Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+#
+
 # Replayer command to execute
 command="sudo ./replay -K 0:ext4:256:jfs:16384"
 
