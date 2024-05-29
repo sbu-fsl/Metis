2f70336f95625c2322c8e44b5ae763b45db85f04
Yifei Liu <yifeliu@cs.stonybrook.edu>
2024-05-29 16:58:15 -0400

    Error handling for mount_driver.sh to reproduce NFS unmount EBUSY

--- fs_bugs/nfs/mount_unmount/mount_driver.sh
+++ fs_bugs/nfs/mount_unmount/mount_driver.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-  
+
+# This script is used to reproduce busy unmount issue in NFSv4 and NFSv3
+# where the NFS server and client are on the same machine
+
+# Pre-defined variables, note that the server and local mount points will be recreated
+# Make sure the ramdisk device and directories are not used by other processes or file systems
 SERVER_MNT_DIR="/mnt/server"
 LOCAL_MNT_DIR="/mnt/local"
 EXT4_RAMDISK="/dev/ram0"
@@ -7,17 +12,31 @@ SLEEP_SECONDS=5
 
 # Set up device and mount points
 setup() {
+    # Load brd ramdisk kernel module
     if lsmod | grep -q "^brd"; then
         echo "brd module is loaded. Unloading it now."
-        rmmod brd
+        if rmmod brd; then 
+            echo "Successfully removed brd module."
+        else
+            echo "Failed to remove brd module."
+            exit 1
+        fi
     fi
 
-    modprobe brd rd_size=256
+    # Load brd module with 256 KiB ramdisk size 
+    if modprobe brd rd_size=256; then
+        echo "Successfully loaded brd module."
+    else
+        echo "Failed to load brd module."
+        exit 1
+    fi
 
+    # Check if the mount point is already mounted, unmount it
     if test -n "$(mount | grep $SERVER_MNT_DIR)" ; then
         umount $SERVER_MNT_DIR || exit $?
     fi
 
+    # Check if the mount point is already mounted, unmount it
     if test -n "$(mount | grep $LOCAL_MNT_DIR)" ; then
         umount $LOCAL_MNT_DIR || exit $?
     fi
@@ -27,50 +46,58 @@ setup() {
         rm -rf $SERVER_MNT_DIR
     fi
 
+    # Remove mount point if not created, and create it again
     if test -d $LOCAL_MNT_DIR ; then
         rm -rf $LOCAL_MNT_DIR
     fi
 
-    mkdir -p $SERVER_MNT_DIR
-    mkdir -p $LOCAL_MNT_DIR
-    chmod 755 $SERVER_MNT_DIR
-    chmod 755 $LOCAL_MNT_DIR
+    # Create mount points and set permissions
+    mkdir -p $SERVER_MNT_DIR || { echo "Failed to create directory $SERVER_MNT_DIR"; exit $?; }
+    mkdir -p $LOCAL_MNT_DIR || { echo "Failed to create directory $LOCAL_MNT_DIR"; exit $?; }
+    chmod 755 $SERVER_MNT_DIR || { echo "Failed to set permissions for $SERVER_MNT_DIR"; exit $?; }
+    chmod 755 $LOCAL_MNT_DIR || { echo "Failed to set permissions for $LOCAL_MNT_DIR"; exit $?; }
 
+    # Check if NFS kernel server is running, start it if not
     if systemctl is-active --quiet nfs-kernel-server; then
         echo "NFS kernel server is already running."
     else
         echo "Starting NFS kernel server..."
         systemctl start nfs-kernel-server
+        # Sleep for a while to make sure the NFS server is started
         sleep 20
         echo "NFS kernel server started."
     fi
 
+    # Create ext4 file system on ramdisk for the NFS server export path
     MKFS_FLAGS="-F -v -E lazy_itable_init=0,lazy_journal_init=0"
     mkfs.ext4 ${MKFS_FLAGS} $EXT4_RAMDISK || exit $?
 }
 
-# Run driver
+# Run the setup function
 setup
 
+# Loop 10 times to reproduce the unmount EBUSY issue
 loop_max=10
 
 for ((i=1; i<=$loop_max; i++)); do
     echo " ---------- Loop ID: $i ---------- "
-        
-    mount -t ext4 $EXT4_RAMDISK $SERVER_MNT_DIR
+    
+    mount -t ext4 $EXT4_RAMDISK $SERVER_MNT_DIR || exit $?
 
-    exportfs -o rw,sync,no_root_squash localhost:$SERVER_MNT_DIR
+    exportfs -o rw,sync,no_root_squash localhost:$SERVER_MNT_DIR || exit $?
+    # Mount with NFSv4 or NFSv3
+    mount -t nfs -o rw,nolock,vers=4,proto=tcp localhost:$SERVER_MNT_DIR $LOCAL_MNT_DIR || exit $?
 
-    mount -t nfs -o rw,nolock,vers=3,proto=tcp localhost:$SERVER_MNT_DIR $LOCAL_MNT_DIR
+    date > $LOCAL_MNT_DIR/date.txt || exit $?
 
-    date > $LOCAL_MNT_DIR/date.txt
+    umount $LOCAL_MNT_DIR || exit $?
+    exportfs -u localhost:$SERVER_MNT_DIR || exit $?
 
-    umount $LOCAL_MNT_DIR
-    exportfs -u localhost:$SERVER_MNT_DIR
-
+    # Try to unmount, if EBUSY, sleep for 5 seconds and try again
     total_sleep=0
     while true; do
-        umount $SERVER_MNT_DIR
+        # Expected to have "target is busy" error here
+        umount $SERVER_MNT_DIR 
 
         if [ $? -eq 0 ]; then
             echo "Unmount succeeded with $total_sleep seconds of sleep."
