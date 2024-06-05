f77026ea5b026b62907131358b6c038545cc2146
Divyaank Tiwari <dtiwari@cs.stonybrook.edu>
2024-06-05 17:22:16 -0400

    Added Copyright notice

--- fs_bugs/jfs/kernel_hang/truncate_sequence_log.py
+++ fs_bugs/jfs/kernel_hang/truncate_sequence_log.py
@@ -1,3 +1,15 @@
+#
+# Copyright (c) 2020-2024 Yifei Liu
+# Copyright (c) 2020-2024 Wei Su
+# Copyright (c) 2020-2024 Divyaank Tiwari
+# Copyright (c) 2020-2024 Erez Zadok
+# Copyright (c) 2020-2024 Stony Brook University
+# Copyright (c) 2020-2024 The Research Foundation of SUNY
+#
+# You can redistribute it and/or modify it under the terms of the Apache License, 
+# Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+#
+
 sequence_log_file_path = 'sequence-pan-20240209-182859-244192_kh6.log'
 truncated_sequence_log_file_path = 'jfs_op_sequence.log'
 
