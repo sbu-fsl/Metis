b300f5558ae2085373f8eb44ea563bf7a5d28413
Divyaank Tiwari <dtiwari@cs.stonybrook.edu>
2024-06-12 18:09:07 -0400

    Added #! at the top and fixed Copyright year

--- fs_bugs/jfs/kernel_hang/truncate_sequence_log.py
+++ fs_bugs/jfs/kernel_hang/truncate_sequence_log.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 
 # Copyright (c) 2020-2024 Yifei Liu
-# Copyright (c) 2020-2024 Divyaank Tiwari
+# Copyright (c) 2023-2024 Divyaank Tiwari
 # Copyright (c) 2020-2024 Erez Zadok
 # Copyright (c) 2020-2024 Stony Brook University
 # Copyright (c) 2020-2024 The Research Foundation of SUNY
@@ -8,6 +9,8 @@
 # You can redistribute it and/or modify it under the terms of the Apache
 # License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
 
+# This python code simply takes a log file having operations for both ext4 and JFS file systems
+# and truncates the log to only have operations from JFS
 sequence_log_file_path = 'jfs_kernel_hang_sequence.log'
 truncated_sequence_log_file_path = 'jfs_op_sequence.log'
 
